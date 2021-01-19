7614886b15de1e71c8773a1397285ec350f4d46b
CCPablo <pablocaravacacalvo@gmail.com>
2021-01-19 14:48:05 +0100

    quick fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,24 @@
 import { search } from './modules/store/music.store.js'
-import { requestCountryCodes } from './modules/controller/music.controller.js'
-import { explicitParam} from './modules/view/filter.js'
+import { requestCountryCodes } from './modules/controller/contries.controller.js'
 
 $(function () {
     requestCountryCodes();
     const searchForm = $('form.search')
-    
-    // searchForm.on('submit', function (event) {
-    //     event.preventDefault()
-    //     let searchSettings = {};
-    //     $(this).find('input, select').each(function(){
-    //         searchSettings[$(this).attr('name')] = $(this).val()
-    //     });
-    //     searchSettings = explicitParam(searchSettings);
-    //     search(searchSettings)
-    // })
 
-    console.log(searchForm.find('input, select'));
     searchForm.find('input, select').on('input', function(){
         let searchSettings = {};
         searchForm.find('input, select').each(function(){
             searchSettings[$(this).attr('name')] = $(this).val()
         });
-        searchSettings = explicitParam(searchSettings);
+        setExplicit(searchSettings);
         search(searchSettings)
     })
+
+    function setExplicit (searchSettings) {
+        if ($("#explicit-filter")[0].checked){
+            searchSettings.explicit="yes";
+        }else{
+            searchSettings.explicit="no";
+        }
+    }
 })
--- src/js/modules/view/render.js
+++ src/js/modules/view/render.js
@@ -1,5 +1,17 @@
 let $results = $('.results')
+let $countryFilter = $('#country-filter')
 
 export function renderResults (results) {
     $results.html(results.map(result => result.html))
+}
+
+export function renderContriesOptions (countryCodes) {
+    countryCodes.map(function (current) {
+        $countryFilter.append(
+            $(
+                `<option value="${current.a2}">${current.nameCurrentValue}</option>`
+            )
+        )
+    })
+    $countryFilter.find("option[value='US']").attr('selected', 'selected')
 }

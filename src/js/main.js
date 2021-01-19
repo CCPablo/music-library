407490dcef17ba8266858c8ea03ddad8cd53a970
Leyber91 <leyber.91.2@gmail.com>
2021-01-19 11:55:22 +0100

    preliminar filter done

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,10 @@ search({
 
 $(function () {
     const searchForm = $('form.search')
-
+    searchForm.find('input, select').on('input', function(event){
+        console.log(event)
+        //if (searchForm){}
+    })
     searchForm.on('submit', function (event) {
         event.preventDefault()
         const searchSettings = {};

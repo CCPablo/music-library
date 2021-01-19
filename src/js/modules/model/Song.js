ae2640610473e5ab1288c9b82b42720dfb9d772b
Leyber91 <leyber.91.2@gmail.com>
2021-01-19 15:29:22 +0100

    preliminar cover flip implemented

--- src/js/modules/model/Song.js
+++ src/js/modules/model/Song.js
@@ -1,7 +1,7 @@
 export class Song {
     constructor (track) {
         this.id = track.trackId
-        this.cover = track.artworkUrl60
+        this.cover = track.artworkUrl100
         this.name = track.trackName
         this.price = track.trackPrice
         this.millisDuration = track.trackTimeMillis
@@ -13,6 +13,96 @@ export class Song {
     }
 
     get html() {
-        return $('<div>').text(this.name)
+        return $('<div>')
+        .append(this.htmlTitle)
+        .append(this.htmlCover)
+        .append(this.htmlPrice)
+        .append(this.htmlSample)
+        //.append(this.htmlDetails)
+        
     }
+
+    get htmlTitle () {
+        return $('<h2>')
+            .addClass('item-title')
+            .text(this.name)
+            .css({
+                fontFamily: '"Boogaloo", cursive',
+            })
+    }
+
+    get htmlCover () {
+        return $('<div>')
+            .addClass('flip-cover')
+            .css({
+                width: '10em',
+                height: '10em',
+                backgroundImage : 'url(' + this.cover + ')',
+                backgroundSize : 'cover'
+            }).append($('<div>')
+                .addClass('flip-cover-inner')
+                .append($('div')
+                    .addClass('flip-cover-front')
+                    .append($('<img>')
+                        .attr('src', this.cover)
+                    )
+                    )
+                .append($('<div>')
+                .addClass('flip-cover-back')
+                .append(this.htmlDetails)
+                )
+            )
+    }
+
+    get htmlPrice (){
+        return $('<a>')
+        .addClass('item-price')
+        .attr("href", this.itunesLink)
+        .append($('<button>')
+            .text(this.price+'$')
+        )
+    }
+
+    get htmlArtist (){
+        return $('<h3>')
+            .addClass('item-artist')
+            .text(this.artistName)
+    }
+
+    get htmlSample (){
+        return $('<a>')
+        .addClass('item-sample')
+        .attr("href", this.audioSample)
+        .append($('<button>')
+            .text('sample')
+        )
+    }
+
+    get htmlDetails (){
+        let min = Math.floor((this.millisDuration/1000/60) << 0)
+        let seconds = Math.floor((this.millisDuration/1000) % 60)
+        return $('<div>')
+            .addClass('item-details')
+            .append($('<div>')
+                .addClass('details-artist')
+                .text('Artist: ' + this.artistName)
+            )
+            .append($('<div>')
+                .addClass('details-collection')
+                .text('Collection: ' + this.collectionName)
+            )
+            .append($('<div>')
+                .addClass('details-release')
+                .text('Release: ' + this.releaseDate.getFullYear())
+            )
+            .append($('<div>')
+                .addClass('details-duration')
+                .text( min + 'min ' + seconds + 's' )
+            )            
+        }
+
+    
+
+
+
 }

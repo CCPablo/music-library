bf395c9088c548b632c5a09acbf77c9ea1ce0ad6
CCPablo <pablocaravacacalvo@gmail.com>
2021-01-19 15:00:13 +0100

    changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,28 @@
 import { search } from './modules/store/music.store.js'
+import { Vinyl } from './modules/components/Vinyl.js'
 
 search({
     term: 'peter+johnson',
 })
+
+const vinyl = new Vinyl();
+$(document.body).append(vinyl.html);
+
+setTimeout(() => {
+    vinyl.rotate(700, 2000)
+    console.log(vinyl.htmlLp)
+}, 2000)
+
+setTimeout(() => {
+    vinyl.rotate(350, 2000)
+}, 4000)
+
+setTimeout(() => {
+    vinyl.resetRotation()
+    vinyl.setImage('https://www.adslzone.net/app/uploads-adslzone.net/2019/04/borrar-fondo-imagen.jpg')
+}, 7000)
+
+
 
 $(function () {
     const searchForm = $('form.search')
